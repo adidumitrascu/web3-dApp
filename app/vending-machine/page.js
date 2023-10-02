dfcf25f835dcfe63eabdf71da06a43be89afd794
Adrian <adriandumitrascu.ro@gmail.com>
2023-10-02 22:56:40 +0200

    add vendingMachine

--- app/vending-machine/page.js
+++ app/vending-machine/page.js
@@ -1,5 +1,24 @@
+import Head from 'next/head'
+import styles from '../vending-machine.module.css'
+import 'bulma/css/bulma.css'
+
+
 export default function VendingMachine() {
   return (
-    <h1>Hello World!</h1>
-  )
+      <div>
+        <Head>
+          <title>VendingMachine App</title>
+        </Head>
+        <nav className="navbar">
+          <div className="container">
+            <div className="navbar-brand">
+              <h1>Vending Machine</h1>
+            </div>
+            <div className='navbar-end'>
+              <button className='button is-primary'>Connect Wallet</button>
+            </div>
+          </div>
+        </nav>
+      </div>
+    )
 }
