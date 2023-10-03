76181fc31e85516a941ca952652356bf167b3e72
Adrian <adriandumitrascu.ro@gmail.com>
2023-10-03 23:36:55 +0200

    bulma css

--- app/vending-machine/page.js
+++ app/vending-machine/page.js
@@ -1,15 +1,11 @@
-import Head from 'next/head'
-import styles from '../vending-machine.module.css'
+import styles from './vending-machine.module.css'
 import 'bulma/css/bulma.css'
 
 
 export default function VendingMachine() {
   return (
-      <div>
-        <Head>
-          <title>VendingMachine App</title>
-        </Head>
-        <nav className="navbar">
+      <div className={styles.main}>
+        <nav className="navbar mt-4 mb-4">
           <div className="container">
             <div className="navbar-brand">
               <h1>Vending Machine</h1>
@@ -19,6 +15,11 @@ export default function VendingMachine() {
             </div>
           </div>
         </nav>
+        <section className={styles.section}>
+          <div className='container'>
+            <p>placeholder text</p>
+          </div>
+        </section>
       </div>
     )
 }
