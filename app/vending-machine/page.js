ecb3ca125c202484d1a6f12ba614e20b019d3d2b
Adrian <adriandumitrascu.ro@gmail.com>
2023-10-07 01:56:34 +0200

    display error message

--- app/vending-machine/page.js
+++ app/vending-machine/page.js
@@ -1,9 +1,27 @@
+"use client"
+import { useState } from 'react'
 import styles from './vending-machine.module.css'
 import Web3 from 'web3'
 import 'bulma/css/bulma.css'
 
+const VendingMachine = () => {
+  let web3
 
-export default function VendingMachine() {
+  const [error, setError] = useState('')
+
+  const connectWalletHandler = async () => {
+    if(typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+      try{
+        await window.ethereum.request({method: "eth_requestAccounts"})
+        web3 = new Web3(window.ethereum)
+      } catch(err) {
+        setError(err.message)
+      }
+    }
+    console.log("Plase install MetaMask")
+  }
+
+
   return (
       <div className={styles.main}>
         <nav className="navbar mt-4 mb-4">
@@ -12,7 +30,7 @@ export default function VendingMachine() {
               <h1 className={styles.h1}>Vending Machine</h1>
             </div>
             <div className='navbar-end'>
-              <button className='button is-primary'>Connect Wallet</button>
+              <button onClick={connectWalletHandler} className='button is-primary'>Connect Wallet</button>
             </div>
           </div>
         </nav>
@@ -21,6 +39,14 @@ export default function VendingMachine() {
             <p>placeholder text</p>
           </div>
         </section>
+        <section className={styles.section}>
+          <div className='container has-text-danger'>
+            <p>{error}</p>
+          </div>
+        </section>
       </div>
     )
 }
+  
+
+export default VendingMachine
