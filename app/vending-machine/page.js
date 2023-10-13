2bebb00d1d3669bea66270f1aa2e52253344736b
Adrian <adriandumitrascu.ro@gmail.com>
2023-10-13 19:13:04 +0200

    connect blockchain wallet to dapp

--- app/vending-machine/page.js
+++ app/vending-machine/page.js
@@ -1,19 +1,50 @@
 "use client"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from './vending-machine.module.css'
 import Web3 from 'web3'
 import 'bulma/css/bulma.css'
+import vmContract from '../../blockchain/vending'
 
 const VendingMachine = () => {
   let web3
 
   const [error, setError] = useState('')
+  const [inventory, setInventory] = useState('')
+  const [myDonutsCount, setMyDonutsCount] = useState('')
+  const [byCount, setByCount] = useState('')
 
-  const connectWalletHandler = async () => {
+  useEffect(() => {
+    getInventoryHandler()
+  })
+
+  const getInventoryHandler = async () => {
+    const inventory = await vmContract.methods.getVendingMachineBalance().call()
+    setInventory(JSON.parse(inventory))
+  }
+
+  const getMyDonutsHandler = async () => {
+    const accounts = await web3.eth.getAccounts()
+    const count = await vmContract.methods.donutBalances(accounts[0]).call()
+    setMyDonutsCount(JSON.parse(count))
+  }
+
+  const updateDonutQty = event => {
+    setByCount(event.target.value)
+  }
+
+  const buyDonutsHandler = async () => {
+    const accounts = await web3.eth.getAccounts()
+    await vmContract.methods.purchase().send({
+      from: account[0]
+    })
+  }
+
+  const connectWalletHandler = async () =>   {
     if(typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
       try{
         await window.ethereum.request({method: "eth_requestAccounts"})
         web3 = new Web3(window.ethereum)
+        getMyDonutsHandler()
       } catch(err) {
         setError(err.message)
       }
@@ -36,7 +67,23 @@ const VendingMachine = () => {
         </nav>
         <section className={styles.section}>
           <div className='container'>
-            <p>placeholder text</p>
+            <h2>Vending machine inventory: {inventory}</h2>
+          </div>
+        </section>
+        <section className={styles.section}>
+          <div className='container'>
+            <h2>My donuts: {myDonutsCount}</h2>
+          </div>
+        </section>
+        <section className={styles.section}>
+          <div className='container mt-5'>
+            <div className="field">
+              <label className="label">Buy donuts</label>
+              <div className="control">
+                <input onChange={updateDonutQty} className="input" type="type" placeholder="Enter amount..."/>
+              </div>
+              <button onClick={buyDonutsHandler} className='button is-primary mt-2'>Buy</button>
+            </div>
           </div>
         </section>
         <section className={styles.section}>
