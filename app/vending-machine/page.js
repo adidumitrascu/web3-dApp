19d4ba82fe3c71f25797c3156cc23278ea2b7971
Adrian <adriandumitrascu.ro@gmail.com>
2023-10-16 23:40:15 +0200

    retrieve list of account

--- app/vending-machine/page.js
+++ app/vending-machine/page.js
@@ -1,58 +1,77 @@
 "use client"
 import { useState, useEffect } from 'react'
-import styles from './vending-machine.module.css'
 import Web3 from 'web3'
+import vendingMachineContract from '../../blockchain/vending'
 import 'bulma/css/bulma.css'
-import vmContract from '../../blockchain/vending'
+import styles from './vending-machine.module.css'
 
 const VendingMachine = () => {
-  let web3
-
+  
   const [error, setError] = useState('')
   const [inventory, setInventory] = useState('')
+  const [successMsg, setSuccessMsg] = useState('')
   const [myDonutsCount, setMyDonutsCount] = useState('')
-  const [byCount, setByCount] = useState('')
+  const [buyCount, setBuyCount] = useState('')
+  const [web3, setWeb3] = useState(null)
+  const [address, setAddress] = useState(null)
+  const [vmContract, setVmContract] = useState(null)
+  const [purchases, setPurchases] = useState(0)
 
   useEffect(() => {
-    getInventoryHandler()
-  })
-
+    if(vmContract) getInventoryHandler()
+    if(vmContract && address) getMyDonutsHandler()
+  }, [vmContract, address, purchases])
+  
   const getInventoryHandler = async () => {
     const inventory = await vmContract.methods.getVendingMachineBalance().call()
-    setInventory(JSON.parse(inventory))
+    setInventory(inventory)
   }
 
   const getMyDonutsHandler = async () => {
-    const accounts = await web3.eth.getAccounts()
-    const count = await vmContract.methods.donutBalances(accounts[0]).call()
-    setMyDonutsCount(JSON.parse(count))
+    const count = await vmContract.methods.donutBalances(address).call()
+    setMyDonutsCount(count)
   }
-
+  
   const updateDonutQty = event => {
-    setByCount(event.target.value)
+    setBuyCount(event.target.value)
   }
 
   const buyDonutsHandler = async () => {
-    const accounts = await web3.eth.getAccounts()
-    await vmContract.methods.purchase().send({
-      from: account[0]
-    })
-  }
 
-  const connectWalletHandler = async () =>   {
+    try {
+      await vmContract.methods.purchase(parseInt(buyCount)).send({
+        from: address,
+        value: web3.utils.toWei('0.02', 'ether') * parseInt(buyCount),
+        gasLimit: 3000000,
+        gasPrice: null
+        
+      })
+     
+      setPurchases(purchases+1)
+      setSuccessMsg(`${buyCount}'donuts purchased!'`)
+    } catch(err){
+      setError(err.message)
+    }
+  }
+  
+  const connectWalletHandler = async () => {
     if(typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
       try{
         await window.ethereum.request({method: "eth_requestAccounts"})
-        web3 = new Web3(window.ethereum)
-        getMyDonutsHandler()
+        const web3 = new Web3(window.ethereum)
+        setWeb3(web3)
+        const accounts = await web3.eth.getAccounts()
+        const vm = vendingMachineContract(web3)
+        setVmContract(vm)
+        setAddress(accounts[0])
       } catch(err) {
         setError(err.message)
       }
+    } else {
+        console.log("Plase install MetaMask")
     }
-    console.log("Plase install MetaMask")
   }
-
-
+  
   return (
       <div className={styles.main}>
         <nav className="navbar mt-4 mb-4">
@@ -67,12 +86,12 @@ const VendingMachine = () => {
         </nav>
         <section className={styles.section}>
           <div className='container'>
-            <h2>Vending machine inventory: {inventory}</h2>
+            <h2>Vending machine inventory: {inventory.toString()}</h2>
           </div>
         </section>
         <section className={styles.section}>
           <div className='container'>
-            <h2>My donuts: {myDonutsCount}</h2>
+            <h2>My donuts: {myDonutsCount.toString()}</h2>
           </div>
         </section>
         <section className={styles.section}>
@@ -89,6 +108,11 @@ const VendingMachine = () => {
         <section className={styles.section}>
           <div className='container has-text-danger'>
             <p>{error}</p>
+          </div>
+        </section>
+        <section className={styles.section}>
+          <div className='container has-text-success'>
+            <p>{successMsg}</p>
           </div>
         </section>
       </div>
